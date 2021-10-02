cc97ae2c49b9bde860c8f546579511082fab9fa0
jorenbroekema <joren.broekema@gmail.com>
2021-10-02 15:45:42 +0200

    use unique db key

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var leveljs = require('level-js');
-var levelup = require('levelup');
-var fs = require('level-filesystem');
+var leveljs = require("level-js");
+var levelup = require("levelup");
+var fs = require("level-filesystem");
 
-var db = levelup('level-filesystem', {db:leveljs});
+var db = levelup(
+  `level-filesystem-${window.crypto.getRandomValues(new Uint32Array(1))[0]}`,
+  { db: leveljs }
+);
 module.exports = fs(db);
