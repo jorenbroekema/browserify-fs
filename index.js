99553ff744908481eae5e3a7a7627ae16bbc7900
jorenbroekema <joren.broekema@gmail.com>
2021-10-02 16:01:52 +0200

    add timestamp to fs id to know order

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var leveljs = require("level-js");
 var levelup = require("levelup");
 var fs = require("level-filesystem");
 
+var now = new Date();
+var secondsSinceEpoch = Math.round(now.getTime() / 1000);
+
 var db = levelup(
-  `level-filesystem-${window.crypto.getRandomValues(new Uint32Array(1))[0]}`,
+  `level-filesystem-${secondsSinceEpoch}-${
+    window.crypto.getRandomValues(new Uint32Array(1))[0]
+  }`,
   { db: leveljs }
 );
 module.exports = fs(db);
